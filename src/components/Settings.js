9a8b9e3f5edc434c8d7cdc2ea9ca45b3da274852
Eric Miranda <ericmiranda7@gmail.com>
2021-06-08 10:00:49 +0530

    add tags settings skeleton

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,5 +1,54 @@
 import { Card, ListGroup } from 'react-bootstrap'
-import DurationForm from './DurationForm'
+import { useSelector, useDispatch } from 'react-redux'
+import { setTimer } from '../reducers/timerReducer'
+
+const styles = {
+  duration: {
+    width: '4em',
+    textAlign: 'right'
+  }
+}
+
+const DurationForm = () => {
+  const timer = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const hoursChangeHandler = (event) => {
+    const duration = Number(event.target.value) * 60 + Number(timer.minutes)
+    dispatch(setTimer(duration))
+  }
+
+  const minutesChangeHandler = (event) => {
+    const duration = Number(event.target.value) + (Number(timer.hours) * 60)
+
+    if (event.target.value > 59) {
+      event.target.value = ''
+    }
+    dispatch(setTimer(duration))
+  }
+
+  return (
+    <div className="d-flex justify-content-end">
+      <span className="d-inline-flex flex-grow-1" style={{alignItems: 'center'}} >Duration:</span>
+      <div className="">
+        <input className="mr-1" type="number" onChange={hoursChangeHandler} style={styles.duration} name="hours" placeholder={timer.hours + 'H'} />
+        <input type="number" onChange={minutesChangeHandler} style={styles.duration} name="minutes" placeholder={timer.minutes + 'M'} />
+      </div>
+    </div>
+  )
+}
+
+const TagSelect = () => {
+  return (
+    <div className="d-flex">
+      <span className="flex-grow-1">Select Tag</span>
+      <select>
+        <option value="Math">Math</option>
+        <option value="Study">Study</option>
+      </select>
+    </div>
+  )
+}
 
 const Settings = () => {
   return (
@@ -10,8 +59,10 @@ const Settings = () => {
           <ListGroup.Item>
             <DurationForm />
           </ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          <ListGroup.Item>
+            <TagSelect />
+          </ListGroup.Item>
+          <ListGroup.Item>Add Tag</ListGroup.Item>
         </ListGroup>
       </Card>
     </div>
