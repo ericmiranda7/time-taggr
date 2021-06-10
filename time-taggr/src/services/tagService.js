e511969fb4e4b8ea433007520fad321fcb245d46
Eric Miranda <ericmiranda7@gmail.com>
2021-06-10 21:31:52 +0530

    frontend ready axios

--- time-taggr/src/services/tagService.js
+++ time-taggr/src/services/tagService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/tags'
+const baseUrl = '/api/tags'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
