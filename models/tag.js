e2d56135bd9d68a191f870f6f55efc582aa88f4c
Eric Miranda <ericmiranda7@gmail.com>
2021-06-10 20:16:02 +0530

    add tag model

---
+++ models/tag.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose')
+
+const tagSchema = new mongoose.Schema({
+  name: String,
+  color: String,
+  isSelected: Boolean,
+  duration: Number,
+  completedTime: Number,
+})
+
+tagSchema.set('toJSON', {
+  transform: (document, returnDoc) => {
+    returnDoc.id = returnDoc._id.toString()
+    delete returnDoc._id
+    delete returnDoc.__v
+  },
+})
+
+const Tag = mongoose.model('Tag', tagSchema)
+
+module.exports = Tag
