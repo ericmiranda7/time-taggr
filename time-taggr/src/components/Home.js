9dc168e05f5cf09ec5d58e666ffd36a80be3de69
Eric Miranda <ericmiranda7@gmail.com>
2021-06-22 16:13:23 +0530

    party poppers

--- time-taggr/src/components/Home.js
+++ time-taggr/src/components/Home.js
@@ -2,17 +2,28 @@ import Timer from './Timer'
 import TagSelect from './settings/TagSelect'
 import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
+import party from "party-js"
 
 const Home = () => {
 	const tags = useSelector(state => state.tags)
+	const timer = useSelector(state => state.timer)
+
+	const selectedTag = tags.find(tag => tag.isSelected)
 
   useEffect(() => {
     window.localStorage.setItem('tags', JSON.stringify(tags))
   }, [tags])
 
+	useEffect(() => {
+		if (timer.expired && selectedTag.value != 'break') {
+			console.log('done')
+			party.confetti(document.getElementById('tagName'))
+		}
+	})
+
 	return (
 		<div className="d-flex flex-column align-items-center mt-0">
-			<div style={{fontSize: '1.2em'}} className="mb-2"><TagSelect width="175px" /></div>
+			<div style={{fontSize: '1.2em'}} className="mb-2" id="tagName"><TagSelect width="175px" /></div>
 			<Timer />
 		</div>
 	)
