a4748d4c5ea32e4f970bc7a899cdec1908f6971e
Eric Miranda <ericmiranda7@gmail.com>
2021-06-23 09:40:19 +0530

    ui improvs

--- time-taggr/src/components/NavigationBar.js
+++ time-taggr/src/components/NavigationBar.js
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   const [navExpanded, setNavExpanded] = useState(false)
 
+  const toggleNav = () => setNavExpanded(!navExpanded)
+
   return (
-    <Navbar bg="dark" expand="lg" variant="dark" onToggle={() => setNavExpanded(!navExpanded)} expanded={navExpanded}>
-      <Link to="/" className="navbar-brand">Time-Taggr</Link>
+    <Navbar id="navToggle" bg="dark" expand="lg" variant="dark" onToggle={toggleNav} expanded={navExpanded} style={{ outline: 'none !important' }}>
+      <Link to="/" className="navbar-brand" onClick={navExpanded ? toggleNav : null}>Time-Taggr</Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto nav" id="navbar">
-          <Link to="/stats" className="nav-link" onClick={() => setNavExpanded(!navExpanded)}>Time Breakdown</Link>
-          <Link to="/settings" className="nav-link" onClick={() => setNavExpanded(!navExpanded)} >Settings</Link>
+          <Link to="/stats" className="nav-link" onClick={toggleNav}>Time Breakdown</Link>
+          <Link to="/settings" className="nav-link" onClick={toggleNav}>Settings</Link>
           <a className="nav-link" href="https://github.com/ericmiranda7/time-taggr">GitHub</a>
         </Nav>
       </Navbar.Collapse>
